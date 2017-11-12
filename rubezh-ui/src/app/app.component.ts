f876e9807a6db1cb8fb3fa5885ef8630adb0d9d2
Dmitry Kiselev <kd.000.000.1@gmail.com>
2017-11-12 22:31:20 +0300

    *

--- rubezh-ui/src/app/app.component.ts
+++ rubezh-ui/src/app/app.component.ts
@@ -1,7 +1,9 @@
 import {Component, OnInit, ViewChild} from '@angular/core';
 
 import {Tree, TreeModule, TreeNode} from 'primeng/primeng';
-import { TreeServiceMock } from './services/tree.service-mock';
+import {TreeServiceMock} from './services/tree.service-mock';
+
+import * as _ from 'lodash';
 
 @Component({
   selector: 'app-root',
@@ -9,7 +11,7 @@ import { TreeServiceMock } from './services/tree.service-mock';
 
     <!--<p-tree [value]="files" selectionMode="single" [(selection)]="selectedFile"></p-tree>-->
 
-    <div class="ui-grid">
+    <div class="ui-grid ui-grid-responsive">
       <div class="ui-grid-col-3">
         <p-tree [value]="tree1" selectionMode="single" [(selection)]="selectedTree1"></p-tree>
       </div>
@@ -18,10 +20,10 @@ import { TreeServiceMock } from './services/tree.service-mock';
       </div>
     </div>
 
-<!--
-    <button pButton type="button" label="Click"></button>
-    <button pButton type="button" label="Danger"  class="ui-button-danger"></button>
--->
+    <!--
+        <button pButton type="button" label="Click"></button>
+        <button pButton type="button" label="Danger"  class="ui-button-danger"></button>
+    -->
   `,
   styles: []
 })
@@ -38,15 +40,49 @@ export class AppComponent implements OnInit {
   tree2: TreeNode[];
   selectedTree2: TreeNode;
 
-  constructor(private treeService: TreeServiceMock) {}
+  constructor(private treeService: TreeServiceMock) {
+  }
 
   ngOnInit() {
-    this.treeService.getTree1().then(tree => this.tree1 = tree);
-    this.treeService.getTree2().then(tree => this.tree2 = tree);
+    Promise.all([
 
-/*
+      this.treeService.getTree1().then(tree => {
+        //console.log(tree);
+        this.tree1 = tree;
+      }),
 
-*/
+      this.treeService.getTree2().then(tree => {
+        //console.log(tree);
+        this.tree2 = tree;
+      })
 
+    ]).then(all => {
+
+        _.forEach(this.tree1, (node) => {
+          this.applyStileRecursive(node);
+        });
+
+        _.forEach(this.tree2, (node) => {
+          this.applyStileRecursive(node);
+        });
+
+      }
+    );
   }
+
+  applyStileRecursive(node: TreeNode) {
+
+    node.expanded = true;
+    node.styleClass = "TreeNodeStyleClassNormal";
+    console.log(node);
+
+    if (node.children) {
+      _.forEach(node.children, (children) => {
+
+        this.applyStileRecursive(children);
+
+      })
+    }
+  }
+
 }
